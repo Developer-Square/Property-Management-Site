3e1e5d17f440146df571affda4286163a936efdc
ryann254 <keddelyronjoz@gmail.com>
2023-02-11 10:38:57 +0300

    feat: add new styling to sidebar

--- src/components/layout/title/index.tsx
+++ src/components/layout/title/index.tsx
@@ -1,17 +1,27 @@
-import React from "react";
-import { useRouterContext, TitleProps } from "@pankod/refine-core";
-import { Button } from "@pankod/refine-mui";
+import React from 'react';
+import { useRouterContext, TitleProps } from '@pankod/refine-core';
+import { Button } from '@pankod/refine-mui';
+
+import { DarkLogo } from 'assets';
 
 export const Title: React.FC<TitleProps> = ({ collapsed }) => {
   const { Link } = useRouterContext();
 
   return (
-    <Button fullWidth variant="text" disableRipple>
-      <Link to="/">
+    <Button fullWidth variant='text' disableRipple>
+      <Link to='/'>
         {collapsed ? (
-          <img src="/refine-collapsed.svg" alt="Refine" width="28px" />
+          <img
+            src={DarkLogo}
+            alt='Logo'
+            style={{ width: '45px', borderRadius: '50%' }}
+          />
         ) : (
-          <img src="/refine.svg" alt="Refine" width="140px" />
+          <img
+            src={DarkLogo}
+            alt='Refine'
+            style={{ marginTop: '10px', width: '50px', borderRadius: '50%' }}
+          />
         )}
       </Link>
     </Button>
