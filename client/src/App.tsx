0b253e9079d0fa43a5342e07a105409aec274f68
ryann254 <keddelyronjoz@gmail.com>
2025-02-11 16:32:49 +0300

    fix: add proper way to add google maps api key in script

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 
 import { Refine, AuthProvider } from '@pankod/refine-core';
 import {
@@ -152,6 +152,19 @@ function App() {
     },
   };
   const { mode } = useContext(ColorModeContext);
+
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+    const script = document.createElement('script');
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Clean up the script on component unmount
+    };
+  }, []);
 
   return (
     <Box
